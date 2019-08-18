e9689f53a491a19cd6eb1154b4b8ee6edaaf0e6f
suresh19493 <suresh19493@gmail.com>
2019-08-18 13:44:28 +0530

    The New file mongodb is added

---
+++ src/a/b/mongDB.java
@@ -0,0 +1,12 @@
+package a.b;
+
+public class MysqlDataBase implements Database {
+
+    public void add(String data) {
+    	System.out.println( data + " is stored in Mysql database");
+    }
+    
+    public void add1(String data) {
+    	System.out.println( data + " is stored in Mysql database");
+    }
+}
